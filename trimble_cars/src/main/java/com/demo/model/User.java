30f5402c45a112d9b3ae10b4d9fbaf4bcc907770
naveennk169 <navinkumar99916@gmail.com>
2025-06-05 17:58:34 +0530

    Update User.java

--- trimble_cars/src/main/java/com/demo/model/User.java
+++ trimble_cars/src/main/java/com/demo/model/User.java
@@ -1,84 +1,53 @@
-package com.demo.model;
+package com.trimblecars.model;
 
 import jakarta.persistence.*;
 
-import java.util.List;
-
 @Entity
-@Table(name = "CUSTOM_USER")
 public class User {
-    @Id
-    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Id @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
     private String username;
     private String password;
-    private String role;  // 'CAR_OWNER', 'END_CUSTOMER', 'ADMIN'
 
-    @OneToMany(mappedBy = "owner")
-    private List<Car> cars;
+    @Enumerated(EnumType.STRING)
+    private Role role;
 
-    @OneToMany(mappedBy = "customer")
-    private List<Lease> leases;
-
-    public User() {
+    public enum Role {
+        OWNER, CUSTOMER, ADMIN
     }
 
-    public User(Long id, String username, String password, String role, List<Car> cars, List<Lease> leases) {
-        this.id = id;
+    // Constructors
+    public User() {}
+    public User(String username, String password, Role role) {
         this.username = username;
         this.password = password;
         this.role = role;
-        this.cars = cars;
-        this.leases = leases;
     }
 
-    public void setId(Long id) {
+    // Getters and setters
+    public Long getId() { 
+        return id; 
+    }
+    public void setId(Long id) { 
         this.id = id;
     }
-
-    public void setUsername(String username) {
-        this.username = username;
-    }
-
-    public void setPassword(String password) {
-        this.password = password;
-    }
-
-    public void setRole(String role) {
-        this.role = role;
-    }
-
-    public void setCars(List<Car> cars) {
-        this.cars = cars;
-    }
-
-    public void setLeases(List<Lease> leases) {
-        this.leases = leases;
-    }
-
-    public Long getId() {
-        return id;
-    }
-
-    public String getUsername() {
+    public String getUsername() { 
         return username;
     }
-
-    public String getPassword() {
-        return password;
+    public void setUsername(String username) { 
+        this.username = username; 
     }
-
-    public String getRole() {
-        return role;
+    public String getPassword() { 
+        return password; 
     }
-
-    public List<Car> getCars() {
-        return cars;
+    public void setPassword(String password) { 
+        this.password = password;
     }
-
-    public List<Lease> getLeases() {
-        return leases;
+    public Role getRole() {
+        return role; 
+    }
+    public void setRole(Role role) { 
+        this.role = role;
     }
 }
-
