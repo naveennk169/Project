9ea04c7c35862399f2457c68f54f06f6cd58e040
naveennk169 <navinkumar99916@gmail.com>
2025-06-05 18:01:39 +0530

    Update User.java

--- trimble_cars/src/main/java/com/demo/model/User.java
+++ trimble_cars/src/main/java/com/demo/model/User.java
@@ -4,7 +4,8 @@ import jakarta.persistence.*;
 
 @Entity
 public class User {
-    @Id @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Id 
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
     private String username;
